6be417b489eeff91a5b19adc1ba2ee8d4aa1303d
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-01-31 11:31:54 -0600

    phase 0 pac 3 function parameters lab

---
+++ index.js
@@ -0,0 +1,25 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+
+
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+  function introduction(name) {
+    return `Hi, my name is ${name}.`
+  }
+
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  }
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  }
